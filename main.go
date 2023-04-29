355286e0c9b2ae27c5d3d75f76845d36e680bc14
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-29 15:25:16 +0430

    Update: main.go

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/monegim/simple-terraform-module/pkg"
+)
+
 func main() {
-	
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return pkg.Provider()
+		},
+	})
 }
