b87d36c60a6948fb12ad5351b7b31d8ee177e28d
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-29 17:16:17 +0430

    Add: provider.go

---
+++ pkg/provider.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"host": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SIMPLE_GIN_HOST", "localhost:8080"),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"simple_gin_book": resourceBooks(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"simple_gin_book": dataSourceBook(),
+		},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	// var host *string
+	return nil, nil
+}
