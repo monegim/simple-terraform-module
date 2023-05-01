257797f33a1ffc78e9aedae0ad0059e645e11522
Mostafa Negim <mostafa.negim@gmail.com>
2023-05-01 08:31:14 +0430

    Add: resourceBook

--- pkg/resource_book.go
+++ pkg/resource_book.go
@@ -10,9 +10,33 @@ import (
 func resourceBook() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceBookCreate,
+		Schema:        map[string]*schema.Schema{
+			"book": &schema.Schema{
+				Type: schema.TypeList,
+				MaxItems: 1,
+				Required: true,
+				Elem: schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type: schema.TypeInt,
+							Required: true,
+						},
+						"title": &schema.Schema{
+							Type: schema.TypeMap,
+						},
+						"author": &schema.Schema{
+							Type: schema.TypeString,
+						},
+						"price": &schema.Schema{
+							Type: schema.TypeInt,
+						},
+					},
+				},
+			},
+		},
 	}
 }
 
 func resourceBookCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	
+	return nil
 }
