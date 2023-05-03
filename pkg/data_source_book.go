e2b427e3faf524087ecd0a77938ea7504ef3d248
Mostafa Negim <mostafa.negim@gmail.com>
2023-05-03 14:52:41 +0430

    Update: data_source_book.go

--- pkg/data_source_book.go
+++ pkg/data_source_book.go
@@ -1,12 +1,9 @@
 package pkg
 
 import (
-	"context"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func dataSourceBook(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return nil, nil
+func dataSourceBook() *schema.Resource {
+	return nil
 }
