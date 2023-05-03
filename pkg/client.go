9c2ae571909653202634901b206424c21f52b8a3
Mostafa Negim <mostafa.negim@gmail.com>
2023-05-03 17:50:03 +0430

    Update: client.go, doRequest method

--- pkg/client.go
+++ pkg/client.go
@@ -1,6 +1,8 @@
 package pkg
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -23,4 +25,21 @@ func NewClient(host *string) (*Client, error) {
 		c.HostURL = *host
 	}
 	return &c, nil
+}
+
+func (c *Client) doRequest(req *http.Request) ([]byte, error) {
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d, body: %s", res.StatusCode, body)
+	}
+	return body, err
 }
