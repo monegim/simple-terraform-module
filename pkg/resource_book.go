5929ad02cf4474f697b7cfb9efa2b205efab796c
Mostafa Negim <mostafa.negim@gmail.com>
2023-05-18 17:55:00 +0430

    Update: Change resource schema

--- pkg/resource_book.go
+++ pkg/resource_book.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -14,31 +15,46 @@ func resourceBook() *schema.Resource {
 		UpdateContext: resourceBookUpdate,
 		DeleteContext: resourceBookDelete,
 		Schema: map[string]*schema.Schema{
-			"book": &schema.Schema{
-				Type:     schema.TypeList,
-				MaxItems: 1,
+			"bookid": &schema.Schema{
+				Type:     schema.TypeInt,
 				Required: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"id": &schema.Schema{
-							Type:     schema.TypeInt,
-							Required: true,
-						},
-						"title": &schema.Schema{
-							Type: schema.TypeString,
-							Required: true,
-						},
-						"author": &schema.Schema{
-							Type: schema.TypeString,
-							Required: true,
-						},
-						"price": &schema.Schema{
-							Type: schema.TypeInt,
-							Required: true,
-						},
-					},
-				},
 			},
+			"title": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"author": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"price": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			// "book": &schema.Schema{
+			// 	Type:     schema.TypeList,
+			// 	MaxItems: 1,
+			// 	Elem: &schema.Resource{
+			// 		Schema: map[string]*schema.Schema{
+			// 			"bookID": &schema.Schema{
+			// 				Type:     schema.TypeInt,
+			// 				Required: true,
+			// 			},
+			// 			"title": &schema.Schema{
+			// 				Type: schema.TypeString,
+			// 				Required: true,
+			// 			},
+			// 			"author": &schema.Schema{
+			// 				Type: schema.TypeString,
+			// 				Required: true,
+			// 			},
+			// 			"price": &schema.Schema{
+			// 				Type: schema.TypeInt,
+			// 				Required: true,
+			// 			},
+			// 		},
+			// 	},
+			// },
 		},
 	}
 }
@@ -47,21 +63,23 @@ func resourceBookCreate(ctx context.Context, d *schema.ResourceData, m interface
 	c := m.(*Client)
 
 	var diags diag.Diagnostics
-	id := d.Get("id").(int)
+	bookID := d.Get("bookid").(int)
 	title := d.Get("title").(string)
 	author := d.Get("author").(string)
 	price := d.Get("price").(int)
 
 	b := Book{
-		ID:     id,
+		BookID: bookID,
 		Title:  title,
 		Author: author,
 		Price:  price,
 	}
 	err := c.CreateBook(b)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.Errorf("something has happened: %s", err)
 	}
+	d.SetId(strconv.Itoa(bookID))
+	setItems(d, b)
 	resourceBookRead(ctx, d, m)
 	return diags
 }
@@ -71,8 +89,10 @@ func resourceBookRead(ctx context.Context, d *schema.ResourceData, m interface{}
 
 	var diags diag.Diagnostics
 
-	bookID := d.Get("id").(int)
-
+	bookID, err := strconv.Atoi(d.Id())
+	if err != nil {
+		diag.FromErr(err)
+	}
 	book, err := c.GetBook(bookID)
 	if err != nil {
 		return diag.FromErr(err)
@@ -89,8 +109,8 @@ func resourceBookDelete(ctx context.Context, d *schema.ResourceData, m interface
 }
 
 func setItems(d *schema.ResourceData, book Book) {
-	d.Set("id", book.ID)
+	d.Set("bookid", book.BookID)
 	d.Set("author", book.Author)
-	d.Set("title", book.Author)
+	d.Set("title", book.Title)
 	d.Set("price", book.Price)
 }
